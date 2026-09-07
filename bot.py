715764d7211d6bddd1f6cacae64c9dac1181e14a
Abdulrahman <abdobaher345@gmail.com>
2026-09-07 23:59:00 +0300

    connect bot to discord

---
+++ bot.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+import discord
+
+
+load_dotenv()
+discord_token = os.getenv("DISCORD_TOKEN")
+football_api_key = os.getenv("FOOTBALL_API_KEY")
+
+
+intents=discord.Intents.default()
+intents.message_content=True
+
+
+client=discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f"{client.user} is online")
+
+client.run(discord_token)
